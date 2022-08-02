973a170c215364bae3e811dd7282be7ff392c5e7
jiancheng <seesawjack14@gmail.com>
2022-08-02 23:09:40 +0800

    0802-環境建置

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Message Board</h1>
+	<form @submit.prevent="addList()">
+		<label>New Message </label>
+		<input type="text" v-model="enterValue">
+		<button>Add Message</button>
+	</form>
+	<h2>Message List</h2>
+	<ul>
+		<li v-for="(item,index) in outputValue" :key="index">
+			<span @click="isDone(item)" :class="{done:item.done}">{{item.finalValue}}</span>
+			<button  @click="removeList(index)">Remove</button>
+		</li>
+	</ul>
+	<h4 v-if="outputValue.length == 0 ">Empty list.</h4>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+		name: 'App',
+		data(){
+			return{
+				enterValue:'',
+				outputValue:[],	
+			}
+		},
+		methods:{
+			addList(){
+				if(!this.enterValue){
+					alert('請輸入內容') 
+					return;
+				} 
+				this.outputValue.push({
+					done:false,
+					finalValue:this.enterValue
+				});
+				sessionStorage.setItem('todoList',JSON.stringify(this.outputValue));
+				this.enterValue= '';
+			},
+			removeList(index){
+				this.outputValue.splice(index,1)
+			},
+			isDone(item){
+				item.done=!item.done;
+				sessionStorage.setItem('todoList',JSON.stringify(this.outputValue));
+			}
+		},
+		beforeMount(){
+			if(sessionStorage.getItem('todoList')){
+				this.outputValue = JSON.parse(sessionStorage.getItem('todoList'))
+			}
+		}
 }
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
