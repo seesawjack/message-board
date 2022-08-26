03a170f1f8b8e0ef540ad648a9f7848e6c96f9a9
DESKTOP-82QL8SF\user <seesawjack14@staff.1111.com.tw>
2022-08-26 16:30:03 +0800

    試圖取得route id參數

--- src/store/index.js
+++ src/store/index.js
@@ -40,7 +40,7 @@ const store =createStore({
                     img:'movie-bn.png',
                     color:'green'
                 }
-            ]
+            ],
         }
     },
     mutations:{
