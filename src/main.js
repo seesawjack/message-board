45d0d74def7e64fb9984aceba2ad9aa0d29fe1f7
DESKTOP-82QL8SF\user <seesawjack14@staff.1111.com.tw>
2022-08-26 14:14:41 +0800

    安裝vuex

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router.js'
 import UUID from 'vue-uuid';
+import store from './store/index.js'
 
 const app = createApp(App)
 app.use(router)
+app.use(store)
 app.use(UUID)
-
-
 app.mount('#app')
